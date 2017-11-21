d7070511254b5ecdec77be6b338219a7ee978cca
Zakaria1159 <z.boumarouane@gmail.com>
2017-11-21 22:15:29 +0000

    footer

--- src/main/webapp/app/layouts/footer/footer.controller.js
+++ src/main/webapp/app/layouts/footer/footer.controller.js
@@ -2,30 +2,15 @@
     'use strict';
 
     angular
-        .module('assessoriaTorrellesApp')
-        .controller('FooterController', FooterController);
+    .module('muturamaApp')
+    .controller('FooterController', FooterController);
 
-    FooterController.$inject = ['Company', 'NgMap','Property'];
 
-    function FooterController (Company, NgMap, Property) {
-        var vm = this;
 
-        NgMap.getMap().then(function(map) {
+   FooterController.$inject = [];
 
-        });
+    function FooterController () {
 
-        loadAll();
-
-        function loadAll() {
-
-            Property.top5(function(result) {
-                vm.propertiesTop5 = result;
-            });
-
-            Company.get(function (result) {
-                vm.company = result;
-            });
-        }
 
     }
 })();
