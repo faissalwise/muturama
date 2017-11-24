3eb945ec1d21f0be1a6d2e84afb3719fb2321aeb
Zakaria1159 <z.boumarouane@gmail.com>
2017-11-24 19:56:43 +0000

    commit 2

---
+++ src/main/java/ma/sobexime/muturama/domain/Agent.java
@@ -0,0 +1,128 @@
+package ma.sobexime.muturama.domain;
+
+import org.hibernate.annotations.Cache;
+import org.hibernate.annotations.CacheConcurrencyStrategy;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.Objects;
+
+/**
+ * A Agent.
+ */
+@Entity
+@Table(name = "agent")
+@Cache(usage = CacheConcurrencyStrategy.NONSTRICT_READ_WRITE)
+public class Agent implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "cin")
+    private String cin;
+
+    @Column(name = "nom")
+    private String nom;
+
+    @Column(name = "prenom")
+    private String prenom;
+
+    @Column(name = "adress")
+    private String adress;
+
+    // jhipster-needle-entity-add-field - JHipster will add fields here, do not remove
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getCin() {
+        return cin;
+    }
+
+    public Agent cin(String cin) {
+        this.cin = cin;
+        return this;
+    }
+
+    public void setCin(String cin) {
+        this.cin = cin;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public Agent nom(String nom) {
+        this.nom = nom;
+        return this;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getPrenom() {
+        return prenom;
+    }
+
+    public Agent prenom(String prenom) {
+        this.prenom = prenom;
+        return this;
+    }
+
+    public void setPrenom(String prenom) {
+        this.prenom = prenom;
+    }
+
+    public String getAdress() {
+        return adress;
+    }
+
+    public Agent adress(String adress) {
+        this.adress = adress;
+        return this;
+    }
+
+    public void setAdress(String adress) {
+        this.adress = adress;
+    }
+    // jhipster-needle-entity-add-getters-setters - JHipster will add getters and setters here, do not remove
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Agent agent = (Agent) o;
+        if (agent.getId() == null || getId() == null) {
+            return false;
+        }
+        return Objects.equals(getId(), agent.getId());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(getId());
+    }
+
+    @Override
+    public String toString() {
+        return "Agent{" +
+            "id=" + getId() +
+            ", cin='" + getCin() + "'" +
+            ", nom='" + getNom() + "'" +
+            ", prenom='" + getPrenom() + "'" +
+            ", adress='" + getAdress() + "'" +
+            "}";
+    }
+}
