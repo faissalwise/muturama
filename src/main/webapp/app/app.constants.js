317ad65cea5b1ae909310b2abbfe29b22068f98e
Zakaria1159 <z.boumarouane@gmail.com>
2017-12-23 20:02:18 +0000

    master zak commit

--- src/main/webapp/app/app.constants.js
+++ src/main/webapp/app/app.constants.js
@@ -5,6 +5,6 @@
         .module('muturamaApp')
         .constant('VERSION', "0.0.1-SNAPSHOT")
         .constant('DEBUG_INFO_ENABLED', false)
-        .constant('BUILD_TIMESTAMP', 1513553803414)
+        .constant('BUILD_TIMESTAMP', 1513890305398)
 ;
 })();
